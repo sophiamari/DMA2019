4911cac83319ed1c7d0ec67590f676c5e1263d00
sophiamari <51975123+sophiamari@users.noreply.github.com>
2019-06-19 09:47:32 -0700

    Uploaded the script.js of project

---
+++ script.js
@@ -0,0 +1,13 @@
+function setPixelColour(pixel)
+{
+  pixel.style.backgroundColor = 'black';
+}
+var penColour = 'black';
+function setPenColour(pen)
+{
+  penColour = pen;
+}
+function setPixelColour(pixel)
+{
+  pixel.style.backgroundColor = penColour;
+}
